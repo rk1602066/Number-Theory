d3034262ad3c12e863b555d11efaf472b773b938
rk1602066 <66769695+rk1602066@users.noreply.github.com>
2020-09-03 22:36:19 -0700

    Update 002seive.cpp

--- Prime Number/002seive.cpp
+++ Prime Number/002seive.cpp
@@ -4,20 +4,22 @@ typedef long long int ll;
 
 void Sieve( ll n){
     bool isPrime[n+1];
+    vector<ll>primes;
     memset(isPrime,true,sizeof(isPrime));
     isPrime[0]=false;isPrime[1]=false;
     for(ll i=2;i<=n;i+=2)isPrime[i]=false;
     isPrime[2]=true;
+    primes.push_back(2);
     for(ll i=3;i<=n;i+=2){
        ll init = i*i;
-       for(ll j=init;j<=n;j+=2*i){
-        isPrime[j]=false;
+       if(isPrime[i]){
+       for(ll j=init;j<=n;j+=2*i)isPrime[j]=false;
+            primes.push_back(i);
        }
     }
     cout<<"All primes between 1 to "<<n<<" are: "<<endl;;
-    for(ll i=2;i<=n;i++){
-        if(isPrime[i])cout<<i<<" ";
-    }
+    for(ll x:primes)
+        cout<<x<<" ";
 }
 
 
@@ -28,4 +30,3 @@ int main()
 	cin>>n;
     Sieve(n);
 }
-
